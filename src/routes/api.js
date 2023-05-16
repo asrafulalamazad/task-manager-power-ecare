38efc6deb98a3c21c4a27eb6b909b3b26206b3af
asrafulalamazad <asrafulalamazad@gmail.com>
2023-05-17 00:30:24 +0600

    update

--- src/routes/api.js
+++ src/routes/api.js
@@ -22,6 +22,7 @@ router.get("/deleteTask/:id", AuthVerifyMiddleware,TaskController.deleteTask);
 router.get("/updateTaskStatus/:id/:status", AuthVerifyMiddleware,TaskController.updateTaskStatus);
 router.get("/listTaskByStatus/:status", AuthVerifyMiddleware,TaskController.listTaskByStatus);
 router.get("/taskStatusCount", AuthVerifyMiddleware,TaskController.taskStatusCount);
+router.get("/",TaskController.taskStatusCount);
 
 
 
