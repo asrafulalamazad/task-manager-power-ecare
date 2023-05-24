2013808be3ed2bbb0e7fbe94fbed3bf251684078
asrafulalamazad <asrafulalamazad@gmail.com>
2023-05-24 23:28:00 +0600

    update

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -7,7 +7,7 @@ exports.registration = (req, res)=>{
     let reqBody= req.body;
     UserModel.create(reqBody,(err,data)=>{
         if(err){
-            res.status(200).json({status:"Fail", data: err})
+            res.status(400).json({status:"Fail", data: err})
         }
         else {
             res.status(200).json({status:"Success", data: data})
